ebe379b5751105cd2fd1db719e9be2210adbe0f6
hardidarmawan <hardidarmawan1420@gmail.com>
2023-03-12 23:55:57 +0700

    feat: membuat main menu pada aplikasi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(" >> APLIKASI PENGHITUNG LUAS BANGUN DATAR <<");
+
+        String[] Menu = {"Hitung Luas Bangun Datar Persegi", "Hitung Luas Bangun Datar Persegi Panjang", "Hitung Luas Bangun Datar Jajar Genjang", "Hitung Luas Bangun Datar Segitiga", "Hitung Luas Bangun Datar Lingkaran", "Keluar"};
+
+        int number = 1;
+
+        for (String menu : Menu) {
+            System.out.println("(" + number + ") " + menu);
+            number++;
+        }
+
+        System.out.print(" >  Pilihan (n): ");
+        int pilihan = input.nextInt() - 1;
+
+        System.out.println("Pilihan anda : " + Menu[pilihan]);
     }
 }
